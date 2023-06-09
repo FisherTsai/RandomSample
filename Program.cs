70290ae48dd6e9a5574c23944c85f8f134dbc6eb
Fisher Tsai <fishy0903@gmail.com>
2023-06-09 18:15:02 +0800

    Add sample project
    Change-Id: Ib0e96ff98182f0d4b92ccac0789e340eb8fdaff6

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿bool done = new Random().Next(0, 11) >= 3;
+if (done == false)
+{
+    return -1;
+}
+
+int count = new Random().Next(0, 11);
+Console.WriteLine(
+    $"<p>成功處理 " +
+    $"<span style=\"color:#00ff00; font-size:x-large\">" +
+    $"{count}" +
+    $"</span> 筆資料。" +
+    $"</p>");
+return 0;
